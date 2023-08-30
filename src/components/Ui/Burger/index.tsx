28eab925340f5d3dacbc00eedda2918828ed8ec5
om-sv <om-sv2@mail.ru>
2023-08-30 19:18:41 +0300

    adptive

--- src/components/Ui/Burger/index.tsx
+++ src/components/Ui/Burger/index.tsx
@@ -9,6 +9,8 @@ import { selectAuth } from '../../../store/Slices/userAuthSlice/selectors';
 const Burger = () => {
     const location = useLocation();
     const isAuth = useTypeSelector(selectAuth);
+    const {data, status} = useTypeSelector(state => state.user);
+    
     const [isActive, setIsActive] = useState<boolean>(false);
 
 
@@ -22,15 +24,21 @@ const Burger = () => {
                     <div className="aside_bg">
                         <img src="	https://mmfafnir.github.io/Blob_Front-end/img/Rectangle%201.jpg" alt="#" />
                     </div>
-                    <div className="aside-person__block">
-                        <div className="aside-person__img">
-                            <img src="img/gde-i-kak-vyrashivat-talanty.png" alt="#" />
-                        </div>
-                        <div className="aside-person__title">
-                            <h2>Фамилия Имя</h2>
-                            <p className="sub_t-tle"> блог front-end разработчика </p>
-                        </div>
-                    </div>
+                    {
+                        isAuth ? (
+                            <div className="aside-person__block">
+                                <div className="aside-person__img">
+                                    <img src="img/gde-i-kak-vyrashivat-talanty.png" alt="#" />
+                                </div>
+                                <div className="aside-person__title">
+                                    <h2>Фамилия Имя</h2>
+                                    <p className="sub_t-tle"> блог front-end разработчика </p>
+                                </div>
+                            </div>
+                        ) : (
+                            <div style={{marginBottom: '30px'}}></div>
+                        )
+                    }
 
                     <div className="aside-links__body">
                         <ul className="aside-links__list">
@@ -47,13 +55,23 @@ const Burger = () => {
                                             <Link to="/works" className={location.pathname === '/works' ? 'active' : ''}>Работы</Link> 
                                         </li>
                                     </>
-                                ) : null
+                                ) : (
+                                    <li className="aside__link"> 
+                                        <Link to="/auth" className={location.pathname === '/works' ? 'active' : ''}>Войти</Link> 
+                                    </li>
+                                )
                             }
                         </ul>
-                        <div className="aside-person__btn aside-person__btn-mg"> 
-                            <a className="my_work aside__btn person__btn" href="Work.html">Мои работы</a>
-                            <button className="write_me aside__btn person__btn" data-modal="contact-modal">Напиши мне</button>
-                        </div>
+                        {
+                            isAuth ? (
+                                <div className="aside-person__btn aside-person__btn-mg"> 
+                                    <Link className="my_work aside__btn person__btn" to="/works">Мои работы</Link>
+                                    <Link className="write_me aside__btn person__btn" to={`${process.env.REACT_APP_API_URL}/user/${data?._id}`}>Мой профиль</Link>
+                                </div>
+                            ) : (
+                                <></>
+                            )
+                        }
                     </div>
 
                 </div>
