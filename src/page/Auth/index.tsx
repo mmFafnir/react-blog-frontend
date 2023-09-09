db93dc0c0614ff7f50f9f123879eed6bcb0152d8
om-sv <om-sv2@mail.ru>
2023-09-09 11:23:52 +0300

    loading auth reg

--- src/page/Auth/index.tsx
+++ src/page/Auth/index.tsx
@@ -1,5 +1,5 @@
 
-import {FC, useState} from 'react';
+import {FC, useState, useTransition} from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { useTypeSelector } from '../../hooks/useTypeSelector';
@@ -8,17 +8,22 @@ import { selectAuth } from '../../store/Slices/userAuthSlice/selectors';
 import FormLogin from '../../components/FormLogin';
 import FormReg from '../../components/FormReg';
 
-
 import './style.css';
+import { Status } from '../../types/Status';
+
 const Auth: FC = () => {
 
     const isAuth = useTypeSelector(selectAuth);
+    const {status} = useTypeSelector(state => state.user);
     const [login, setLogin] = useState<boolean>(true);
 
     if(isAuth) return <Navigate to={'/'} />
 
     return (
         <div className='auth'>
+            <div className={`auth__loading ${status === Status.LOADING ? 'show' : ''}`}>
+                <span className='loader'></span>
+            </div>
             {
                 login ? <FormLogin /> : <FormReg /> 
             }
